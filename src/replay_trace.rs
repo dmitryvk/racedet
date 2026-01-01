d0fe324c1a5976fa0f055d26a116e3ce76776d4a
Dmitrii Kalianov <Kalyanov.Dmitry@gmail.com>
2026-01-01 21:21:08 +0100

    wip replay

--- src/replay_trace.rs
+++ src/replay_trace.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 use crate::{
@@ -7,40 +9,44 @@ use crate::{
 
 #[derive(Debug, Clone)]
 pub struct ReplayTrace {
+    strings: StringPool,
     steps: Vec<ReplayStep>,
 }
 
 #[derive(Debug, Clone)]
+struct StringPool {
+    vec: Vec<String>,
+    map: HashMap<String, StringIdx>,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct StringIdx(usize);
+
+#[derive(Debug, Clone)]
 pub struct ReplayStep {
     suspended_tasks: Vec<SuspendedTask>,
-    resumed_tasks: Vec<TaskRef>,
+    resumed_tasks: Vec<TaskStableId>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct TaskRef {
-    pub id: TaskStableId,
-    pub name: String,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct SuspendedTask {
-    pub id: TaskStableId,
-    pub name: String,
-    pub position: String,
+#[derive(Debug, Clone)]
+struct SuspendedTask {
+    id: TaskStableId,
+    name: StringIdx,
+    position: StringIdx,
 }
 
 impl ReplayTrace {
     pub(crate) fn from_trace(trace: &Trace) -> Self {
-        Self {
-            steps: trace
-                .trace
-                .iter()
-                .filter_map(Self::step_from_item)
-                .collect(),
-        }
+        let mut strings = StringPool::new();
+        let steps = trace
+            .trace
+            .iter()
+            .filter_map(|item| Self::step_from_item(item, &mut strings))
+            .collect();
+        Self { strings, steps }
     }
 
-    fn step_from_item(item: &TraceItem) -> Option<ReplayStep> {
+    fn step_from_item(item: &TraceItem, strings: &mut StringPool) -> Option<ReplayStep> {
         let TraceItem::ScheduleDecision {
             resumed_tasks,
             running_tasks: _,
@@ -55,19 +61,17 @@ impl ReplayTrace {
                 .iter()
                 .map(|task| SuspendedTask {
                     id: task.id.stable_id().expect("suspended tasks have stable id"),
-                    name: task.name.clone(),
-                    position: task
-                        .position
-                        .clone()
-                        .expect("suspended tasks have position"),
+                    name: strings.intern(&task.name),
+                    position: strings.intern(
+                        task.position
+                            .as_ref()
+                            .expect("suspended tasks have position"),
+                    ),
                 })
                 .collect(),
             resumed_tasks: resumed_tasks
                 .iter()
-                .map(|task| TaskRef {
-                    id: task.id.stable_id().expect("resumed tasks have stable id"),
-                    name: task.name.clone(),
-                })
+                .map(|task| task.id.stable_id().expect("resumed tasks have stable id"))
                 .collect(),
         };
         Some(step)
@@ -97,11 +101,46 @@ impl std::fmt::Display for ReplayTrace {
                 resumed = step
                     .resumed_tasks
                     .iter()
-                    .map(|task| format!("{id}-{name}", id = task.id, name = task.name,))
+                    .map(|id| format!("{id}"))
                     .join(","),
             )?;
             written = true;
         }
+        write!(f, "#")?;
+        let mut written = false;
+        for string in &self.strings.vec {
+            if written {
+                write!(f, ",")?;
+            }
+            // TODO: escaping
+            write!(f, "{string}")?;
+            written = true;
+        }
         Ok(())
+    }
+}
+
+impl StringPool {
+    fn new() -> Self {
+        Self {
+            vec: Vec::new(),
+            map: HashMap::new(),
+        }
+    }
+
+    fn intern(&mut self, value: &str) -> StringIdx {
+        if let Some(idx) = self.map.get(value) {
+            return *idx;
+        };
+        let idx = StringIdx(self.vec.len() + 1);
+        self.vec.push(value.to_owned());
+        self.map.insert(value.to_owned(), idx);
+        idx
+    }
+}
+
+impl std::fmt::Display for StringIdx {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
